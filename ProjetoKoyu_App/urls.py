6c07c4e31516e2a4e9aa5f6747f5c8581e7306a6
Shwipy <andresequeira99@gmail.com>
2024-12-10 09:37:45 +0000

    Dashboard/Listar_Users - views/url

--- ProjetoKoyu_App/urls.py
+++ ProjetoKoyu_App/urls.py
@@ -4,5 +4,8 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('listar_utilizadores', views.listar_utilizadores, name='listar_utilizadores'),
 ]
+
 
--- ProjetoKoyu_App/views.py
+++ ProjetoKoyu_App/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request, 'projeto_koyu/index.html', context={'message': 'Bem-vindo!'})
+
+def dashboard(request):
+    return render(request, 'projeto_koyu/dashboard.html')
+
+def listar_utilizadores(request):
+    return render(request, 'projeto_koyu/listar_utilizadores.html')
