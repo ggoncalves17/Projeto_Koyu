665ab0ef3d460f346d43006cbaf4dc31b823a8f2
Guilherme Gonçalves <guigonzo.1@hotmail.com>
2024-12-17 10:07:55 +0000

    Fix: Views.py

--- ProjetoKoyu_App/views.py
+++ ProjetoKoyu_App/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
-from django.contrib.auth.models import Utilizador
+from .models import Utilizador
+from .forms import UtilizadorForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 from django.http import HttpResponse
