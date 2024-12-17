3b05e9460b9545dbe8bdc764da14c016c059561e
Guilherme Gonçalves <guigonzo.1@hotmail.com>
2024-12-17 09:50:36 +0000

    Login View

--- ProjetoKoyu_App/views.py
+++ ProjetoKoyu_App/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
-from django.contrib.auth.models import User
+from django.contrib.auth.models import Utilizador
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 from django.http import HttpResponse
