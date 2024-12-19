389d96f02f313f4c1cc012b1c0eb94fba707c29a
David Janeiro <a2022131358@alunos.estgoh.ipc.pt>
2024-12-19 09:36:32 +0000

    Atualização Treino

--- ProjetoKoyu/urls.py
+++ ProjetoKoyu/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin 
-from django.urls import path, include 
+from django.urls import path, include
 
+from ProjetoKoyu_App import views 
 
 urlpatterns = [ 
     path('admin/', admin.site.urls),  
     path("", include('ProjetoKoyu_App.urls')),
-]
-
-    
+]  
--- ProjetoKoyu_App/urls.py
+++ ProjetoKoyu_App/urls.py
@@ -4,5 +4,7 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('adicionarExercicio/', views.AdicionarExercicioView.as_view(), name='Adicionar-Exercicio'),
+    path('adicionarTreino/', views.AdicionarTreinoView.as_view(), name='Adicionar-Treino'),
 ]
 
