394379dd9c02130fccadd63d3f8a28faf3c56fdd
Shwipy <andresequeira99@gmail.com>
2025-01-17 22:13:25 +0000

    Dashboard Finalizado + Alterações no Menu

--- ProjetoKoyu_App/views.py
+++ ProjetoKoyu_App/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, authenticate
-from .models import Utilizador, UtilizadorManager
+from .models import Equipamento, Exercicios, Historico, PlanoTreinos, Utilizador, UtilizadorManager
 from .forms import UtilizadorForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
@@ -47,7 +47,33 @@ def login_view(request):
 
 @login_required
 def dashboard(request):
-    return render(request, 'projeto_koyu/dashboard.html')
+    #Menu
+    if request.method == "POST":
+        exercicios = request.POST.get("exercicios")
+        treinos = request.POST.get("treinos")
+        utilizadores = request.POST.get("utilizadores")
+        perfil = request.POST.get("perfil")
+        
+        if utilizadores: 
+            return redirect("/listar_utilizadores")
+
+    #Estatisticas
+    utilizadores = Utilizador.objects.all()
+    n_utilizadores = len(utilizadores)
+    exercicios = Exercicios.objects.all()
+    n_exercicios = len(exercicios)
+    treinosrealizados = Historico.objects.all()
+    n_treinosrealizados = len(treinosrealizados)
+
+    #Historico
+    ultimos_treinos = PlanoTreinos.objects.order_by('-id')[:5]
+    ultimos_exercicios = Exercicios.objects.order_by('-id')[:5]
+    ultimos_users = Utilizador.objects.order_by('-id')[:5]
+    ultimos_equipamentos = Equipamento.objects.order_by('-id')[:5]
+
+    return render(request, 'projeto_koyu/dashboard.html',{'n_utilizadores':n_utilizadores,'n_exercicios':n_exercicios,'n_treinosrealizados':n_treinosrealizados,
+                                                          'ultimos_treinos':ultimos_treinos,'ultimos_exercicios':ultimos_exercicios,
+                                                          'ultimos_users':ultimos_users,'ultimos_equipamentos':ultimos_equipamentos})
 
 #Funcoes relativas a pagina listar utilizadores
 @login_required
